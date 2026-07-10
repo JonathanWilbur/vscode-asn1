6c158889f2d457c0c0acae35d90efc58bcb4ef7c
Jonathan M. Wilbur <jonathan@wilbur.space>
2026-07-09 20:37:58 -0400

    test: document symbols

---
+++ src/test/symbols.test.ts
@@ -0,0 +1,50 @@
+import * as assert from 'node:assert/strict';
+import * as vscode from 'vscode';
+
+const ASN1_MODULE_A: string = `
+SymbolsTest DEFINITIONS ::= BEGIN
+IMPORTS int1 FROM SymbolsA;
+
+int2 INTEGER ::= int1
+
+ErrorResponse{INTEGER:defaultCode} ::= SEQUENCE {
+    code INTEGER DEFAULT defaultCode,
+    message UTF8String OPTIONAL
+}
+
+BoofyError ::= ErrorResponse{int1}
+
+CoolNumbers INTEGER ::= {int1 | int2, ...}
+
+MESSAGE ::= TYPE-IDENTIFIER
+
+msg1 MESSAGE ::= { PrintableString IDENTIFIED BY int1 }
+
+-- This should NOT match, despite the substring.
+blint1 INTEGER ::= 4
+
+int3 INTEGER ::= SymbolsA.int2
+
+END
+`;
+
+suite('Document Symbols', function () {
+    // You have to use a regular function (not arrow) for `this` to be defined properly.
+    // this.timeout(10000);
+    test('provides one symbol for each assignment', async () => {
+        const ext = vscode.extensions.getExtension<{ indexingPromise: Promise<void> }>("wildboar.asn1")!;
+        const outcome = await ext.activate();
+        await outcome.indexingPromise;
+        const document = await vscode.workspace.openTextDocument({
+            language: "asn1",
+            content: ASN1_MODULE_A,
+        });
+        const symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
+            "vscode.executeDocumentSymbolProvider",
+            document.uri,
+        );
+        assert.equal(symbols.length, 1); // one for the module
+        const modsymbol = symbols[0];
+        assert.equal(modsymbol.children.length, 9); // one import and eight assignments
+    });
+});
